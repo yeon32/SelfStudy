af742eb5c0370f474aaa7f2ac61e45c432544a66
yeon32 <32jyeon@gmail.com>
2022-03-20 15:46:12 +0900

    add nav_MainPage

---
+++ src/components/views/MainPage/MainPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navigation from '../components/nav_MainPage';
+import Footer from '../components/Footer';
+
+
+function MainPage() {
+    return (
+      <div className="MainPage">
+        <Navigation /> 
+
+        
+         
+        <Footer />
+         
+        
+        </div>
+          
+        );
+       }
+  
+  export default MainPage;
