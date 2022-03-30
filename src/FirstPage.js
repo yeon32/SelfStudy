efb88b89f3197cc45a3df637aa93bbf0d2da8203
yeon32 <32jyeon@gmail.com>
2022-03-30 15:49:34 +0900

    로그인페이지 수정

--- src/FirstPage.js
+++ src/FirstPage.js
@@ -13,45 +13,44 @@ import Footer from "./Footer.js";
 
 function FirstPage() {
     return (
-      <div className="FirstPage">
-         <header>
+      <><div className="FirstPage">
+          <header>
 
-      <div className={styles.navcolor}>     
-      <Navbar>
-           <Container>
-              <img src={logo} alt="로고" className={styles.logo}/>
-             
-             <Navbar.Brand>공부는 셀프입니다</Navbar.Brand>
-             <Nav className="me-auto">
-               <Nav.Link href="/Introduction">서비스소개</Nav.Link>
-               <Nav.Link href="/Ranking">랭킹</Nav.Link>
-               <Nav.Link href="/MakeProblem"><Badge bg="primary">문제만들기</Badge></Nav.Link>
-               </Nav>
-               </Container>
-               </Navbar>
-               </div> 
-               </header>  
-                
+             <div className={styles.navcolor}>
+                <Navbar>
+                   <Container>
+                      <img src={logo} alt="로고" className={styles.logo} />
 
-               <div>
-                <Login />
-               </div>
-               
-              
-               <div className={styles.firstpage_logo}>
-               <img
-                  src={firstpage_logo}            
-                  />
+                      <Navbar.Brand>공부는 셀프입니다</Navbar.Brand>
+                      <Nav className="me-auto">
+                         <Nav.Link href="/Introduction">서비스소개</Nav.Link>
+                         <Nav.Link href="/Ranking">랭킹</Nav.Link>
+                         <Nav.Link href="/MakeProblem"><Badge bg="primary">문제만들기</Badge></Nav.Link>
+                      </Nav>
+                   </Container>
+                </Navbar>
+             </div>
+          </header>
+
+
+          <div>
+             <Login />
+          </div>
+
+
+          <div className={styles.firstpage_logo}>
+             <img
+                src={firstpage_logo} /> 
+
+       </div>
+      
                   
-                  </div>
-                  <Footer/>
-                  </div>
 
-               
+        </div></>     
           
      );
 
-  }
+    }
     
 
   export default FirstPage;
--- src/Login.js
+++ src/Login.js
@@ -1,34 +1,81 @@
-import React from 'react';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import ReactDOM from 'react-dom';
+
 import styles from "./Login.module.css";
 import google from "./images/google.png";
 import naver from "./images/naver.png";
 
+
+
+
 function Login() {
-    return(
-      <><div className={styles.loginas}>
-        <h1>Logged in as:</h1>
-      </div>
-      <div className="auth_social_buttons" form style={{display: 'flex', flexDirection: 'column'}}>
-
-          <button className={styles.auth_google_button}>
-            <a href="/oauth2/authorization/google">
-              <div className={styles.auth_naver_button_image}>
-              <img src={google} alt="google" />
-              </div>
-            </a>
-          </button>
-
-          <button className={styles.auth_naver_button}>
-            <a href="/oauth2/authorization/naver">
-              <div className={styles.auth_naver_button_image}>
-                <img src={naver} alt="naver" />
-              </div>
-            </a>
-          </button>
+  return (
 
 
-        </div></>
+
+
+
+    );
+  }
+  
+      
+  export default Login;
+    
+  
+
+
+
+
+  
+   /* <><div className={styles.loginas}>
+    <h1>Logged in as:</h1>
+  </div>
+  <div className="auth_social_buttons" form style={{display: 'flex', flexDirection: 'column'}}>
+
+      <button className={styles.auth_google_button}>
+        <a href="/oauth2/authorization/google">
+          <div className={styles.auth_naver_button_image}>
+          <img src={google} alt="google" />
+          </div>
+        </a>
+      </button>
+
+      <button className={styles.auth_naver_button}>
+        <a href="/oauth2/authorization/naver">
+          <div className={styles.auth_naver_button_image}>
+            <img src={naver} alt="naver" />
+          </div>
+        </a>
+      </button>
+
+
+</div></>*/
+      
+         
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  
 
       //구글/네이버 로그인 연동하면 MainPage로 넘어가도록 연동
 
@@ -54,7 +101,4 @@ function Login() {
     */ 
     
 
-    )
-}
-
-export default Login;
+  
