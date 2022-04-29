78e06f9ab096abe7a7fb3cf3b5f3dab8fe8402f3
yeon32 <32jyeon@gamil.com>
2022-04-29 19:05:28 +0900

    solveproblem 수정

--- src/SolveProblem.js
+++ src/SolveProblem.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, Component} from 'react';
 import { InputGroup, Container, FormControl, Button, Dropdown, NavItem } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Header';
-import styles from './MakeProblem.module.css'
+import styles from './SolveProblem.module.css'
 import Footer from './Footer.js';
 import axios from 'axios';
 import useSWR from 'swr';
@@ -15,23 +15,15 @@ function SolveProblem () {
     <Header />
     <div className={styles.text}>문제 풀기!</div>
 
+    <div className={styles.section}>
     <div className={styles.content}>
     <label>✔정답</label>
             <div>
                 <input type="text" size={90}  name="solve" placeholder="정답 작성하기" className={styles.hight} />
             </div>
-    </div>
 
-    <div className={styles.beforebutton}>
-      <Button variant="primary" size="lg">
-        이전 
-      </Button>
-     </div> 
-     <div className={styles.afterbutton}>
-      <Button variant="primary" size="lg">
-        다음
-      </Button>
-     </div>
+    </div>
+    </div>
 
      <Footer />    
     
